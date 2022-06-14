614e4c193944362572e562cd56a8a2b1acbfd47c
Michael Edelstone <michael.edelstone@spr.com>
2022-06-14 18:37:00 +0000

    Merged PR 231: Adds stylized empty state to various blank areas
    Not sure this is the *most* optimal way to create a empty state component in the long run, but it does replace the basic placeholder with a better look.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
 
 const routes: Routes = [
-  {
-    path: 'dashboard',
-    loadChildren: () =>
-      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
-  },
+  { path: 'dashboard', component: TodoComponent },
   {
     path: 'cash',
     loadChildren: () => import('./cash/cash.module').then((m) => m.CashModule),
   },
   { path: 'financialInstruments', component: TodoComponent },
-  {
-    path: 'payments',
-    loadChildren: () =>
-      import('./payments/payments.module').then((m) => m.PaymentsModule),
-  },
+  { path: 'payments', component: TodoComponent },
   { path: 'reports', component: TodoComponent },
   { path: 'dataManagement', component: TodoComponent },
   { path: 'administration', component: TodoComponent },
