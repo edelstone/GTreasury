e8e5a77c4ec924021f7571ebfa8b6e6dcb12da9b
Michael Edelstone <michael.edelstone@spr.com>
2022-06-29 01:12:24 +0000

    Merged PR 305: Adds a multiselect to the payments filter toolbar
    adds a multiselect to the payments filter toolbar

--- src/app/payments/components/approvals-grid/approvals-toolbar/approvals-toolbar.component.ts
+++ src/app/payments/components/approvals-grid/approvals-toolbar/approvals-toolbar.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ApprovalsToolbarComponent {
 
+  readonly templateType = ['Repetitive', 'Non-repetitive', 'Free form'];
+  templateTypeDefault = ['Repetitive', 'Non-repetitive', 'Free form'];
+
   constructor() { }
 
 }
